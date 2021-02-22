c066de1bca4d0d8a15a9bb0b66829eb5f2fc12c4
angusjf <el17ajf@leeds.ac.uk>
2021-02-22 20:49:19 +0000

    work on integrating multiple views

--- src/Main.elm
+++ src/Main.elm
@@ -5,57 +5,24 @@ import Browser.Events
 import Element exposing (Element)
 import Element.Input
 import Element.Font
-import CustomElement as CElement
-import Visualiser
 import Config exposing (Config)
-import Graph exposing (Graph)
 import VsColor
-import Instance exposing (Instance)
-import Package.JavaToGraph
-import Package.Visualiser
-import Package.Graph
-import Project.JavaToGraph
-import Project.Visualiser
-import Project.Graph
+import Combined
+import File exposing (File, Uri)
 
-type alias Uri = String
-
-type alias File =
-  { uri : Uri
-  , content : String
+type alias Model =
+  { config : Config
+  , combined : Combined.Model
   }
 
-type Menu
- = Main
- | SelectFiles
- | SelectGraph
-
-type alias Model n e =
-  { files : List (File, Bool)
-  , config : Config
-  , fromSources : List String -> List (String, Graph n e)
-  , instance : Instance n e (Visualiser.Msg n)
-  , menu : Menu
-  , selectedGraphAndVisualiser : Maybe (String, (Visualiser.Model n e))
-  , graphs : List (String, Graph n e)
-  }
-
-type Msg n e
+type Msg
   = NewFile File
   | UpdateFile File
   | DeleteFile Uri
   | RenameFile (Uri, Uri)
   | ConfigChanged Config
-  | VisualiserMsg (Visualiser.Msg n)
   | Tick Float
-  | ViewMenu Menu
-  | SetFileSelected File Bool
-  | GraphSelected (String, Graph n e)
-
-type alias Current n e =
-  { instance : Instance n e (Visualiser.Msg n)
-  , fromSources : List String -> List (String, Graph n e)
-  }
+  | CombinedMsg Combined.Msg
 
 port newFile : (File -> msg) -> Sub msg
 port updateFile : (File -> msg) -> Sub msg
@@ -64,180 +31,67 @@ port renameFile : ((Uri, Uri) -> msg) -> Sub msg
 port configChanged : (Config -> msg) -> Sub msg
 port infoMessage : String -> Cmd msg
 
-temp_log : List (File, Bool) -> List (File, Bool)
-temp_log fs =
+init : Config -> (Model, Cmd (Msg))
+init config =
   let
-    _ = List.map (\({uri}, _) -> Debug.log "-> " uri) fs
+      (cModel, cMsg) = Combined.init config
   in
-    fs
+      ({ config = config
+       , combined = cModel
+       }
+      , Cmd.map CombinedMsg cMsg
+      )
 
-toGraph : (List String -> List (String, Graph n e)) -> List (File, Bool)
-                                          -> String -> Maybe (Graph n e)
-toGraph fromSources files name =
-  files
-  |> filesToStrings
-  |> fromSources
-  |> List.filter (\(n, _) -> n == name)
-  |> List.head
-  |> Maybe.map Tuple.second
+view : Model -> Browser.Document (Msg)
+view model =
+    { title = "Visualiser"
+    , body =
+        [ Element.layout
+            [ VsColor.fontColor VsColor.Foreground
+            , Element.Font.size 13
+            , Element.padding 4
+            ]
+            (Element.map CombinedMsg (Combined.view model.combined))
+        ]
+    }
 
-filesToStrings : List (File, Bool) -> List String
-filesToStrings files =
-  files
-  |> List.filterMap (\(f, b) -> if b then Just f else Nothing)
-  |> List.map .content
-
-init =
-  init2
-      {-
-        Project.JavaToGraph.fromSources
-        { viewNode = Project.Visualiser.viewNode
-        , viewEdge = Project.Visualiser.viewEdge
-        , onClick  = Project.Visualiser.onClick
-        , getRect  = Project.Visualiser.getRect
-        }
-      -}
-        Package.JavaToGraph.fromSources
-        { viewNode = Package.Visualiser.viewNode
-        , viewEdge = Package.Visualiser.viewEdge
-        , onClick  = Package.Visualiser.onClick
-        , getRect  = Package.Visualiser.getRect
-        }
-          
-init2 : (List String -> List (String, Graph n e))
-     -> Instance n e (Visualiser.Msg n)
-     -> Config
-     -> (Model n e, Cmd (Msg n e))
-init2 fromSources instance config =
-  ({ files = []
-   , config = config
-   , fromSources = fromSources
-   , menu = Main
-   , graphs = []
-   , selectedGraphAndVisualiser = Nothing
-   , instance = instance
-   }
-  , Cmd.none
-  )
-
-isJust : Maybe a -> Bool
-isJust = Maybe.withDefault False << Maybe.map (always True)
-
-update : Msg n e -> Model n e -> (Model n e, Cmd (Msg n e))
+update : Msg -> Model -> (Model, Cmd (Msg))
 update msg model =
   case msg of 
     NewFile file ->
-      setFiles (insert file model.files) model
+      setFiles (insert file model.combined.files) model
     UpdateFile file ->
-      setFiles (insert file model.files) model
+      setFiles (insert file model.combined.files) model
     DeleteFile uri ->
-      setFiles (delete uri model.files) model
+      setFiles (delete uri model.combined.files) model
     RenameFile (from, to) ->
-      setFiles (rename from to model.files) model
-    VisualiserMsg vMsg ->
-      case model.selectedGraphAndVisualiser of
-        Just (n, old) ->
-          let
-            (new, msg_) = Visualiser.update model.config vMsg old
-          in
-            ( { model
-                | selectedGraphAndVisualiser = Just (n, new)
-              }
-            , Cmd.map VisualiserMsg msg_
-            )
-        Nothing ->
-            ( model
-            , Cmd.none
-            )
+      setFiles (rename from to model.combined.files) model
     ConfigChanged cfg ->
       ( { model
           | config = cfg
-          , selectedGraphAndVisualiser = 
-              Maybe.map
-                (\(s, v) -> (s, Visualiser.withConfig cfg v))
-                model.selectedGraphAndVisualiser
+          , combined =
+              let
+                  c = model.combined
+              in
+                { c | config = cfg }
         }
       , Cmd.none
       )
     Tick _ ->
-      ( applyToVis Visualiser.tick model
+      ( { model | combined = Combined.tick model.combined }
       , Cmd.none
       )
-    ViewMenu menu ->
-      ( { model | menu = menu }
-      , Cmd.none
-      )
-    SetFileSelected file selected ->
+    CombinedMsg cMsg ->
       let
-        files =
-          model.files
-          |> List.map (\(f, s) -> if f.uri == file.uri
-                                    then (file, selected) else (f, s))
+        (cModel, cMsg2) = Combined.update cMsg model.combined
       in
-        setFiles files model
-    GraphSelected (name, graph) ->
-      ( { model
-          | menu = Main
-          , selectedGraphAndVisualiser =
-             Just
-               ( name
-               , Visualiser.init
-                   model.config
-                   graph
-                   model.instance
-               )
-        }
-      , Cmd.none
+      ( { model | combined = cModel }
+      , Cmd.map CombinedMsg cMsg2
       )
 
-setFiles : List (File, Bool) -> Model n e -> (Model n e, Cmd (Msg n e))
+setFiles : List (File, Bool) -> Model -> (Model, Cmd (Msg))
 setFiles files model =
-  let
-    graphs = model.fromSources <| filesToStrings <| files
-    newModel = 
-      { model
-        | files = files
-        , graphs = graphs
-        , selectedGraphAndVisualiser =
-              case model.selectedGraphAndVisualiser of
-                  Just x -> Just x
-                  Nothing ->
-                    Maybe.map
-                        (\(n, graph) ->
-                            ( n
-                            , Visualiser.init
-                                model.config
-                                graph
-                                model.instance
-                            )
-                        )
-                        (List.head graphs)
-      }
-  in
-    case model.selectedGraphAndVisualiser of
-        Just (selectedGraph, vis) ->
-            case toGraph model.fromSources files selectedGraph of
-                Just graph ->
-                    ( { newModel
-                        | selectedGraphAndVisualiser =
-                            Just <|
-                                ( selectedGraph
-                                , Visualiser.withGraph model.config graph vis
-                                )
-                      }
-                    , Cmd.none
-                    )
-                Nothing ->
-                    ( newModel
-                    , let
-                        msg =
-                          case selectedGraph of
-                            "" -> "Compilation failed for unnamed package"
-                            g  -> "Compilation failed for package '" ++ g ++ "'"
-                      in infoMessage msg
-                    )
-        Nothing ->
-            (newModel, Cmd.none)
+    update (CombinedMsg (Combined.SetFiles files)) model
 
 insert : File -> List (File, Bool) -> List (File, Bool)
 insert file files =
@@ -266,131 +120,7 @@ rename from to files =
         else (x, b) :: rename from to xs
     [] -> []
 
-applyToVis : (Visualiser.Model n e -> Visualiser.Model n e) -> Model n e
-                                                            -> Model n e
-applyToVis f model =
-  case model.selectedGraphAndVisualiser of
-    Just (selectedGraph, visualiser) ->
-        { model
-          | selectedGraphAndVisualiser = Just (selectedGraph, f visualiser)
-        }
-    Nothing -> model
-
-view : Model n e -> Browser.Document (Msg n e)
-view model =
-  { title = "Visualiser"
-  , body = [ Element.layout
-             [ VsColor.fontColor VsColor.Foreground
-             , Element.Font.size 13
-             , Element.padding 4
-             ] <|
-             CElement.column <|
-               case model.selectedGraphAndVisualiser of
-                 Just (_, vis) -> 
-                   [ Visualiser.view model.config vis
-                     |> Element.html
-                     |> Element.map VisualiserMsg
-                   , viewOverlay model
-                   ]
-                 Nothing ->
-                   [ viewNoGraphError
-                   ]
-           ]
-  }
-
-viewNoGraphError : Element (Msg n e)
-viewNoGraphError =
-  CElement.text "Sorry! I couldn't find any Java classes in that workspace"
-
-viewSelectGraph : List (String, Graph n e) -> Element (Msg n e)
-viewSelectGraph options =
-  CElement.column <|
-    [ CElement.text "Please select a package to continue:"
-    , CElement.column <|
-        List.map
-          (\(name, graph) ->
-               CElement.button
-                 { onPress = Just <| GraphSelected (name, graph)
-                 , label = name
-                 }
-          )
-          options
-    ]
-
-viewOverlay : Model n e -> Element (Msg n e)
-viewOverlay model =
- case model.menu of
-   SelectFiles ->
-       viewSelectFilesPopup model.files
-   SelectGraph ->
-       viewSelectGraph model.graphs
-   Main ->
-       CElement.column
-         [ CElement.button 
-           { onPress = Just (ViewMenu SelectFiles)
-           , label = "Select Files..."
-           }
-         , case model.selectedGraphAndVisualiser of
-             Just (selectedGraph, vis) ->
-               CElement.column
-                 [ CElement.button 
-                   { onPress = Just (ViewMenu SelectGraph)
-                   , label = case selectedGraph of
-                               "" -> "Unnamed Package"
-                               g  -> "Package: '" ++ selectedGraph ++ "'"
-                   }
-                 , Visualiser.viewOverlay vis
-                       |> Element.map VisualiserMsg
-                 ]
-             Nothing ->
-               Element.column [] []
-         ]
-
-viewSelectFilesPopup : List (File, Bool) -> Element (Msg n e)
-viewSelectFilesPopup files =
-  CElement.column <|
-    (List.map viewFileSelect files) ++ 
-    [ CElement.button
-      { onPress = Just (ViewMenu Main)
-      , label = "back"
-      }
-    ]
-
-viewFileSelect : (File, Bool) -> Element (Msg n e)
-viewFileSelect (file, sel) =
-  Element.row
-    [ Element.spacing 8 ]
-    [ CElement.text <| trimUntilRev (\c -> c == '/') file.uri
-    , toggleFileButton (file, sel)
-    ]
-
-trimUntilRev fn str =
-  let prefix = String.reverse <| trimUntil fn <| String.reverse str
-  in String.replace prefix "" str
-
-trimUntil : (Char -> Bool) -> String -> String
-trimUntil f str =
-  let
-    helper : List Char -> List Char
-    helper chars =
-      case chars of 
-        x::xs -> if f x then xs
-                        else helper xs
-        [] -> []
-  in
-    str
-    |> String.toList
-    |> helper
-    |> String.fromList
-
-toggleFileButton : (File, Bool) -> Element (Msg n e)
-toggleFileButton (file, sel) =
-  CElement.button
-    { onPress = Just <| SetFileSelected file (not sel)
-    , label = if sel then "✓" else "✕"
-    }
-
-subscriptions : Model n e -> Sub (Msg n e)
+subscriptions : Model -> Sub (Msg)
 subscriptions model =
   Sub.batch
     [ newFile NewFile
@@ -408,3 +138,4 @@ main =
     , update = update
     , subscriptions = subscriptions
     }
+
